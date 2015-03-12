19c06b91881bb37c1b3c7f312d52b6c612c89dc3
Dongdong Tian <seisman.info@gmail.com>
2015-03-12 10:05:35 +0800

    yum without sudo

--- install.sh
+++ install.sh
@@ -3,74 +3,74 @@ set -e
 ############ Basic: System ##############
 function basic_system()
 {
-    sudo yum install -y epel-release                   # Adding EPEL Repo
-    sudo yum update  -y                                # Update system
+    yum install -y epel-release                   # Adding EPEL Repo
+    yum update  -y                                # Update system
 }
 
 
 ############ Basic: Runtime ##############
 function basic_runtime()
 {
-    sudo yum install -y gcc                            # Install C compiler
-    sudo yum install -y gcc-c++                        # Install C++ compiler
-    sudo yum install -y gcc-gfortran                   # Install Fortran compiler
-    sudo yum install -y make                           # Install make tool
-    sudo yum install -y gdb                            # Install gdb debug tool
-    sudo yum install -y cmake                          # Install cmake tool
-    sudo yum install -y git                            # Install git version control tool
+    yum install -y gcc                            # Install C compiler
+    yum install -y gcc-c++                        # Install C++ compiler
+    yum install -y gcc-gfortran                   # Install Fortran compiler
+    yum install -y make                           # Install make tool
+    yum install -y gdb                            # Install gdb debug tool
+    yum install -y cmake                          # Install cmake tool
+    yum install -y git                            # Install git version control tool
 }
 
 
 ############ Basic: Tools ################
 function basic_tools()
 {
-    sudo yum install -y p7zip                          # Install support to .7z or .zip file
-    sudo yum install -y lrzsz                          # Install support to sz and rz command
-    sudo yum install -y zsh                            # Install zsh 
-    sudo chsh -s $(which zsh)                          # Set zsh as the default shell
-    sudo yum install -y autojump                       # Install autojump
-    sudo yum install -y autojump-zsh                   # Install autojump for zsh
+    yum install -y p7zip                          # Install support to .7z or .zip file
+    yum install -y lrzsz                          # Install support to sz and rz command
+    yum install -y zsh                            # Install zsh 
+    chsh -s $(which zsh)                          # Set zsh as the default shell
+    yum install -y autojump                       # Install autojump
+    yum install -y autojump-zsh                   # Install autojump for zsh
 }
 
 ############ Advanced: Java ##############
 function adv_java()
 {
-    sudo yum install -y java                           # Install Java runtime
+    yum install -y java                           # Install Java runtime
 }
 
 
 ########### Advanced: Python #############
 function adv_python()
 {
-    sudo yum install -y python-pip                     # Install Python pip package tool 
-    sudo yum install -y python-matplotlib              # Install Matplotlib drawing library
-    sudo yum install -y numpy                          # Install Numpy matrix calculation library
-    sudo yum install -y scipy                          # Install Scipy scientific calculation library
-    sudo yum install -y python-requests                # Install Requests webpage library
-    sudo yum install -y python-lxml                    # Install lxml webpage analyse library
-    sudo yum install -y python-beautifulsoup4          # Install BeautifulSoup webpage analyse library
-    sudo yum install -y python-docopt                  # Install Docopt commandline library
-    sudo yum install -y python-argparse                # Install Argparse commandline library
-    sudo yum install -y ipython                        # Install iPython runtime
+    yum install -y python-pip                     # Install Python pip package tool 
+    yum install -y python-matplotlib              # Install Matplotlib drawing library
+    yum install -y numpy                          # Install Numpy matrix calculation library
+    yum install -y scipy                          # Install Scipy scientific calculation library
+    yum install -y python-requests                # Install Requests webpage library
+    yum install -y python-lxml                    # Install lxml webpage analyse library
+    yum install -y python-beautifulsoup4          # Install BeautifulSoup webpage analyse library
+    yum install -y python-docopt                  # Install Docopt commandline library
+    yum install -y python-argparse                # Install Argparse commandline library
+    yum install -y ipython                        # Install iPython runtime
 }
 
 ########### Advanced: PHP ################
 function adv_php()
 {
-    sudo yum install -y php                            # Install PHP
-    sudo yum install -y php-mysql                      # Install MySQL support for PHP
-    sudo yum install -y php-fpm                        # Install PHP-fpm
-    sudo yum install -y php-redis                      # Install Redis support for PHP
+    yum install -y php                            # Install PHP
+    yum install -y php-mysql                      # Install MySQL support for PHP
+    yum install -y php-fpm                        # Install PHP-fpm
+    yum install -y php-redis                      # Install Redis support for PHP
 }
 
 
 ############ Advanced: Tools #############
 function adv_tools()
 {
-    sudo yum install -y httpd                          # Install Apache server
-    sudo yum install -y redis                          # Install Redis
-    sudo yum install -y mysql-server                   # Install MySQL database server
-    sudo yum install -y mongodb                        # Install Mongodb database server
+    yum install -y httpd                          # Install Apache server
+    yum install -y redis                          # Install Redis
+    yum install -y mysql-server                   # Install MySQL database server
+    yum install -y mongodb                        # Install Mongodb database server
 }
 
 
